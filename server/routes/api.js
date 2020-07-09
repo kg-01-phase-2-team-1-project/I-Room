bb3882888c2fc75d66bf99346d500048c8bcdb3c
julioj27 <juliojaya27@gmail.com>
2020-07-09 22:33:57 +0700

    edit .env

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const apiController = require('../controllers/apiController.js');
+const ApiController = require('../controllers/ApiController.js');
 
 
 
--- server/routes/room.js
+++ server/routes/room.js
@@ -1,20 +1,20 @@
 const express = require('express');
 const router = express.Router();
 
-const roomController = require('../controllers/roomController.js');
+const RoomController = require('../controllers/RoomController.js');
 
-router.get('/', roomController.getRoomRootHandler);
-router.post('/add', roomController.postRoomAddHandler);
+router.get('/', RoomController.getRoomRootHandler);
+router.post('/add', RoomController.postRoomAddHandler);
 
-router.get('/update/:id', roomController.getRoomUpdateHandler);
+router.get('/update/:id', RoomController.getRoomUpdateHandler);
 
 // update room type
-router.put('/update/:id', roomController.putRoomUpdateHandler);
+router.put('/update/:id', RoomController.putRoomUpdateHandler);
 
 // soft delete - update room status
-router.put('/delete/:id', roomController.putRoomDeleteHandler);
+router.put('/delete/:id', RoomController.putRoomDeleteHandler);
 
 // booking - update room status (customer view)
-router.put('/booking/:id', roomController.putRoomBookingHandler);
+router.put('/booking/:id', RoomController.putRoomBookingHandler);
 
 module.exports = router;
